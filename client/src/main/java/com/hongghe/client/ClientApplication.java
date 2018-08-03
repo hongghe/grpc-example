73dbadf02a7b808bea363d9fda441730787881e2
hongghe <364930855@qq.com>
2018-08-03 21:10:13 +0800

    初始化的配置信息

--- client/src/main/java/com/hongghe/client/ClientApplication.java
+++ client/src/main/java/com/hongghe/client/ClientApplication.java
@@ -2,9 +2,30 @@ package com.hongghe.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+/**
+ * 启动文件
+ *
+ * @author hongghe 03/08/2018
+ */
 @SpringBootApplication
 public class ClientApplication {
+
+	@Bean
+	public ClientInitializer clientInitializer() {
+		return new ClientInitializer();
+	}
+
+	@Bean
+	public ClientProperties clientProperties() {
+		return new ClientProperties();
+	}
+
+	@Bean
+	public ClientWebConfig clientWebConfig() {
+		return new ClientWebConfig();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientApplication.class, args);
--- server/src/main/java/com/hongghe/server/ServerApplication.java
+++ server/src/main/java/com/hongghe/server/ServerApplication.java
@@ -2,9 +2,30 @@ package com.hongghe.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+/**
+ * 启动文件
+ *
+ * @author hongghe 03/08/2018
+ */
 @SpringBootApplication
 public class ServerApplication {
+
+	@Bean
+	public ServerInitializer serverInitializer() {
+		return new ServerInitializer();
+	}
+
+	@Bean
+	public ServerProperties serverProperties() {
+		return new ServerProperties();
+	}
+
+	@Bean
+	public ServerWebConfig serverWebConfig() {
+		return new ServerWebConfig();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
