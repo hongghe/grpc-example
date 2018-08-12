b94697832f0fa81fb9e45dd65d79a79db1e459ca
hongghe <364930855@qq.com>
2018-08-12 17:56:10 +0800

    初始化的配置信息

--- client/src/main/java/com/hongghe/client/ClientProperties.java
+++ client/src/main/java/com/hongghe/client/ClientProperties.java
@@ -1,5 +1,6 @@
 package com.hongghe.client;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
@@ -7,6 +8,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  *
  * @author hongghe 03/08/2018
  */
-@ConfigurationProperties(prefix = "client")
+@ConfigurationProperties(prefix = "client_demo")
+@Data
 public class ClientProperties {
 }
---
+++ client/src/main/java/com/hongghe/client/domain/ApiResult.java
@@ -0,0 +1,36 @@
+package com.hongghe.client.domain;
+
+/**
+ * 接口返回结果信息
+ *
+ * @author hongghe 03/08/2018
+ */
+public enum ApiResult {
+
+    ERROR(-1, "ERROR"),
+    SUCCESS(1, "SUCCESS");
+
+    private Integer code;
+    private String message;
+
+    ApiResult(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
